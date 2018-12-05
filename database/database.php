590d6a0879822d65b8b5cfcffc22276ce1cb19fd
khuynh16 <38110561+khuynh16@users.noreply.github.com>
2018-12-05 10:17:46 -0500

    Added values to variables; changed var names

--- database/database.php
+++ database/database.php
@@ -1,6 +1,6 @@
 <?php
-$hostname = '127.0.0.1:3306';
-$username = 'root';
-$password = 'mysql';
-$database = 'userAndPassword';
+$db_hostname = 'fairpay.cp5vn5hxqyhg.us-east-2.rds.amazonaws.com';
+$db_username = 'FairPayAdmin';
+$db_password = 'FairPay1';
+$database = 'FairPay';
  ?>
