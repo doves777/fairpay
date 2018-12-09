194332e1ac4536b017104143be9510b4cdbbf793
khuynh16 <38110561+khuynh16@users.noreply.github.com>
2018-12-08 20:43:34 -0500

    "Remove item" page, actions to ...ItemsConfirm.php file

---
+++ ManagerPageRemoveItems.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>FairPay</title>
+<link rel="stylesheet" href="styleLogin.css"> 
+
+</head>
+
+<body>
+
+<h1 id="reset"> Remove Service </h1>
+
+	Current List of Available Services:
+	<br><br>
+
+<form action="ManagerPageRemoveItemsConfirm.php" method="post" onsubmit="return confirm('Are you sure you want to remove this service?');">	
+	
+	<?php	
+		// calls php file holding credentials to sql database on AWS
+		require_once('database.php');
+		
+		// variable of connection to database on AWS
+		$conn = mysqli_connect($db_hostname, $db_username, $db_password, $database);
+		
+		// sql statement to get usernames of all currently employed members; to populate dropdown menu
+		$sql = "SELECT service_name FROM Services WHERE available = 'Y'";
+		// store sql result into result variable
+		$result = mysqli_query($conn, $sql);
+		
+		//drop down list of employee usernames from MySQL table on AWS
+		echo '<select id="nameField" name="user">';
+		while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
+			echo '<option value="'.$row['service_name'].'">'.$row['service_name'].'</option>';
+		}
+		echo '</select>';// Close your drop down box
+			
+		$sql = "UPDATE Services SET available = 'Y' WHERE service_name = 'test2'";
+		$result = mysqli_query($conn, $sql);			
+	?>
+	
+	<br>
+	
+	<input type="submit" value="Remove">
+	
+	<br><br>
+
+</form>
+	
+	<button onclick="goBack()">Back</button>
+
+<script>
+
+	//function to go back to previous page
+	function goBack() {
+	window.history.back();
+	}
+
+</script>
+
+</body>
+</html>
