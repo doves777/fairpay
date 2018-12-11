b85d451c8eeabcd7c77b524dc13649032e8518ba
khuynh16 <38110561+khuynh16@users.noreply.github.com>
2018-12-11 14:06:54 -0500

    Update login.php

--- login.php
+++ login.php
@@ -10,14 +10,6 @@
 			<form action="process.php" method="POST">
 				<h1 class="slideDown">FairPay</h1>
 				
-				<!-- <p>
-					<label>Username:</label>
-					<input type="text" id="user" name="user" />
-				</p> -->
-				<!-- Above commented code replaced with below code -->
-				
-				 
-				
 				<?php
 				
 				// calls php file holding credentials to sql database on AWS
@@ -40,43 +32,16 @@
 			
 				?>
 				
-				
-				
-				<!-- Sai's Code 
-				
-				<div id="nameField" class="slideDown">
-					 <input list="employees" name="user" id="user" required>
-					 <datalist id="employees">
-						<option value="Manager">
-						<option value="Joe">
-						<option value="Dan">
-						<option value="Bob">
-						<option value="Alex">
-						<option value="Chris">
-					</datalist>
-				</div>
-				->
-				
-				
-				
-				<!-- <p>
-					<label>Password:</label>
-					<input type="password" id="pass" name="pass" />
-				</p> -->
-				<!-- Above commented code replaced with below code -->
 				<div id="passwordField" class="slideDown">
 					<input type="password" placeholder="Password" name="pass" id="pass" required />
 				</div>			
 				
-				<!-- <p>
-					<input type="submit" id="btn" value="Login" />
-				</p> -->
-				<!-- Above commented code replaced with below code -->
 				<div>
-					<input type="submit" value="Log in" id="myButton" class="slideDown"/>
+					<input type="submit" value="Log in" id="myButton" class="slideDown" />
 				</div>
 			</form>
 		</section>
 	</div>
+	
 </body>
 </html>
